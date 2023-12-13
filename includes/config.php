16b7636f42a79b6560fdf53eb03f6d17c0dfb1d8
blacksyclone <144563235+blacksyclone@users.noreply.github.com>
2023-12-13 15:52:39 -0500

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', '192.168.0.16' );          // Set database host
+  define( 'DB_USER', 'apache2' );             // Set database user
+  define( 'DB_PASS', 'granyayo1' );             // Set database password
+  define( 'DB_NAME', 'dbInventario' );        // Set database name
+?>
